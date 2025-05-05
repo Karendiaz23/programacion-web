97dbfc29e76ca2d3224bc2b08e8f0e5081f3f389
kareendiaz <kareendiaz23@gmailcom>
2025-05-05 15:54:50 -0300

    actividad de array

--- javascript/metodos-arrays/actividad.js
+++ javascript/metodos-arrays/actividad.js
@@ -1,20 +1,24 @@
 //actividad//
 
 
-let producto =[];
-producto.push({ nombre: "Celular", precio: 500, categoria: "Electrónica" });
-producto.push({ nombre: "Zapatillas", precio: 100, categoria: "Ropa" });
-producto.push({ nombre: "Cafetera", precio: 80, categoria: "Electrodomésticos" });
+let productos =[];
+productos.push({ nombre: "Celular", precio: 500, categoria: "Electrónica" });
+productos.push({ nombre: "Zapatillas", precio: 100, categoria: "Ropa" });
+productos.push({ nombre: "Cafetera", precio: 80, categoria: "Electrodomésticos" });
 
 //forEach
 console.log("Todos los productos:");
-producto.forEach(inventario => {
+productos.forEach(inventario => {
   console.log(inventario);
 });
 
 //filter
-let electronicos = producto.filter(inventario => producto.categoria === "Electrónica");
+let electronicos = productos.filter(producto => producto.categoria === "Electrónica");
 console.log("Productos electrónicos:");
 console.log(electronicos);
 
 //array donde uso map
+let nombresProductos = productos.map(producto => producto.nombre);
+console.log("Nombres de los productos:");
+console.log(nombresProductos); 
+
