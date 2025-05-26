e4bd64ca108d0653d3b2de9fd0be14f4b1f78051
kareendiaz <kareendiaz23@gmail.com>
2025-05-26 16:29:10 -0300

    apuntes

---
+++ javascript/pruebaa.js
@@ -0,0 +1,8 @@
+//declaracion de variables
+
+//var=resignarles sus valores,vive en culaquier scop
+let redefinible= 1; //reasignable sus valores 
+//const= es constante
+
+//for y for EACH RECORREN PERO EL FOR ES FLEXIBLE
+//MAP Y FILTER DEVUELVEN UN VALOR
